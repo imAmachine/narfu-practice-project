e0754e90cb0c3d4adff8f9517fd23e027dfc37f6
GuerdySSS <sanya.lyubimov.9500@mail.ru>
2023-06-11 16:33:36 +0300

    fetch

--- static/scripts/slider.js
+++ static/scripts/slider.js
@@ -1,28 +1,49 @@
-let btnLeft = document.getElementsByClassName('slider__btn_left')[0]
-let btnRight = document.getElementsByClassName('slider__btn_right')[0]
-let sliderImages = document.querySelectorAll('.slider__img')
-let imgLength = document.getElementsByClassName('slider__img')[0].offsetWidth
-let counter = 1
-let startShift = 0
-let gap = 10
+document.addEventListener('DOMContentLoaded', async function() {
+  let sliderWrapper = document.getElementsByClassName("slider__wrapper")[0]
+  let response = await fetch('/api/dormitories')
+  if (response.ok) {
+    let json = await response.json()
+    for(let i=0; i<json.length; i++) {
+      let img = document.createElement('img')
+      img.className = 'slider__img'
+      img.src = json[i]['photo']
+      img.id = json[i]['dormitory_id']
+      sliderWrapper.appendChild(img)
+    }
 
-let sliderWrapper = document.getElementsByClassName("slider__wrapper")[0]
-let countMinus = Math.floor(sliderWrapper.offsetWidth / imgLength)
-let sliderShiftRight = () => {
-  if (counter <= sliderImages.length - countMinus) {
-    counter += 1
-    startShift -= imgLength + gap
-    sliderImages.forEach(el => el.style.transform = `translateX(${startShift}px)`)
+    let btnLeft = document.getElementsByClassName('slider__btn_left')[0]
+    let btnRight = document.getElementsByClassName('slider__btn_right')[0]
+    let sliderImages = document.querySelectorAll('.slider__img')
+    // let imgLength = document.querySelector('.slider__img').offsetWidth
+    let counter = 1
+    let startShift = 0
+    let gap = 10
+    let countMinus = Math.floor(sliderWrapper.offsetWidth / 200)
+
+    let sliderShiftRight = () => {
+      if (counter <= sliderImages.length - countMinus) {
+        counter += 1
+        startShift -= 200 + gap
+        sliderImages.forEach(el => el.style.transform = `translateX(${startShift}px)`)
+      }
+    }
+
+    let sliderShiftLeft = () => {
+      if (counter > 1) {
+        counter -= 1
+        startShift += 200 + gap
+        sliderImages.forEach(el => el.style.transform = `translateX(${startShift}px)`)
+      }
+    }
+
+    btnLeft.onclick = sliderShiftLeft
+    btnRight.onclick = sliderShiftRight
+
+    sliderImages.forEach(el => el.addEventListener('click', () => getInfo(el.id)));
+
+    let getInfo = id => {
+      console.log(id)
+    }
   }
-}
-
-let sliderShiftLeft = () => {
-  if (counter > 1) {
-    counter -= 1
-    startShift += imgLength + gap
-    sliderImages.forEach(el => el.style.transform = `translateX(${startShift}px)`)
-  }
-}
-
-btnLeft.onclick = sliderShiftLeft
-btnRight.onclick = sliderShiftRight
+  else alert("Ошибка HTTP: " + response.status)
+})
