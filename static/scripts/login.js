94b7ef045a9c3da5aabe04d3d5a7a7023eafcf61
GuerdySSS <sanya.lyubimov.9500@mail.ru>
2023-06-14 23:57:52 +0300

    the end 2

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,35 +1,44 @@
-const buttonAuth = document.querySelector('.buttonAuth');
-const closeImgs = document.querySelectorAll('.close_img');
-const btns = document.querySelectorAll('.loginForm__wrapper__btn');
-const inputEyes = document.querySelectorAll('.loginForm__wrapper__inputWrapper__eye');
-const images = document.querySelectorAll('.main__wrapper__left__img');
+const loginForm = document.querySelector('.loginForm')
+const buttonAuth = document.querySelector('.buttonAuth')
+const closeImgs = document.querySelectorAll('.close_img')
+const registration = document.querySelector('.loginForm__wrapper__registration')
+const login = document.querySelector('.loginForm__wrapper__login')
 
 
-// Изменения поле пароля с text на password и наоборот + смена класса картинки глаза (обычный и зачеркнутый)
-const togglePasswordVisibility = (inputEye, passwordInput) => {
-  const passwordInputType = passwordInput.getAttribute('type');
-  const newInputType = passwordInputType === 'password' ? 'text' : 'password';
-  inputEye.src = inputEye.src.includes('_line')
-    ? inputEye.src.replace('_line', '')
-    : inputEye.src.replace('eye', 'eye_line');
-  passwordInput.setAttribute('type', newInputType);
-};
-
-const activateImageLink = (img) => {
-  images.forEach((img) => img.classList.remove('main__wrapper__left__img_active'));
-  img.classList.add('main__wrapper__left__img_active');
+// разворот картонки с регистрацией / логином
+const rotateCardAuth = () => {
+  registration.classList.toggle('loginForm__wrapper__registration_rotate')
+  login.classList.toggle('loginForm__wrapper__login_rotate')
 };
 
 
-// переворот картонки при регистрации / авторизации
-btns.forEach((btn) => btn.addEventListener('click', rotateCardAuth));
+// получаем кнопки, которые и должны переворачивать картонку с логином и пароолем
+const btns = document.querySelectorAll('.loginForm__wrapper__btn')
 
+// навешиваем на них метод переворота
+btns.forEach(btn => btn.addEventListener('click', rotateCardAuth))
+
+
+// убираем / добавляем класс для глазика (обычный или зачеркнутый) + поле для пароля делаем type text / password
+// делается для двух инпутов и глаз, т.к. каждый элемент расположен на обеих сторонах
+const inputEyes = document.querySelectorAll('.loginForm__wrapper__inputWrapper__eye')
 inputEyes.forEach((inputEye, index) => {
-  const passwordInput = document.getElementsByClassName('loginForm__wrapper__input')[index * 2 + 1];
-  inputEye.addEventListener('click', () => togglePasswordVisibility(inputEye, passwordInput));
+  const passwordInput = document.getElementsByClassName('loginForm__wrapper__input')[index * 2 + 1]
+  inputEye.addEventListener('click', () => {
+    const passwordInputType = passwordInput.getAttribute('type')
+    inputEye.src = inputEye.src.includes('_line') ? inputEye.src.replace('_line', '') : inputEye.src.replace('eye', 'eye_line')
+    passwordInput.setAttribute('type', passwordInputType === 'password' ? 'text' : 'password')
+  });
 });
 
-buttonAuth.addEventListener('click', openCloseForm);
-closeImgs.forEach((closeImg) => closeImg.addEventListener('click', openCloseForm));
+// метод закрытия картонки логина / пароля
+const openCloseForm = () => {
+  loginForm.classList.toggle('loginForm_hide')
+  document.body.classList.toggle('body__overflowHidden')
+};
 
-images.forEach((img) => img.addEventListener('click', () => activateImageLink(img)));
+// получаем обе кнопки закрытия картонки
+buttonAuth.addEventListener('click', openCloseForm)
+
+// навешиваем на них метод закрытия
+closeImgs.forEach(closeImg => closeImg.addEventListener('click', openCloseForm))
